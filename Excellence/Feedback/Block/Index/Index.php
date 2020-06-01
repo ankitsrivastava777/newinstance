74cb0c415a5cfad74325ae9555142910f2d4a122
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-06-01 14:24:53 +0530

    compleated atofill name email using function created.

--- Excellence/Feedback/Block/Index/Index.php
+++ Excellence/Feedback/Block/Index/Index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Excellence\Feedback\Block\Index;
 
+
 class Index extends \Magento\Framework\View\Element\Template
 {
     /**
@@ -12,15 +13,38 @@ class Index extends \Magento\Framework\View\Element\Template
      * @param \Magento\Framework\View\Element\Template\Context  $context
      * @param array $data
      */
+    protected $_customerSession;
+    protected $_customer;
+
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\SessionFactory $customerSession,
+
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->_customerSession = $customerSession->create();
     }
     public function _prepareLayout()
     {
         $this->pageConfig->getTitle()->set(__('Feedback'));
         return parent::_prepareLayout();
+        $this->_customerFactory = $customerFactory;
+    }
+    public function getLoggedinCustomerId()
+    {
+        if ($this->_customerSession->isLoggedIn()) {
+            return $this->_customerSession->getId();
+        }
+        return false;
+    }
+
+    public function getCustomerData()
+    {
+        if ($this->_customerSession->isLoggedIn()) {
+            return $this->_customerSession->getCustomerData();
+        }
+        return false;
     }
 }
