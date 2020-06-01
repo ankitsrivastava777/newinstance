72f7e8419b251e35d8d7286ed65c450294f9466e
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-06-01 14:27:53 +0530

    compleated auofill using function

--- Excellence/Feedback/Block/Index/Index.php
+++ Excellence/Feedback/Block/Index/Index.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Excellence\Feedback\Block\Index;
 
-
 class Index extends \Magento\Framework\View\Element\Template
 {
     /**
@@ -16,7 +15,6 @@ class Index extends \Magento\Framework\View\Element\Template
     protected $_customerSession;
     protected $_customer;
 
-
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Customer\Model\SessionFactory $customerSession,
@@ -26,12 +24,14 @@ class Index extends \Magento\Framework\View\Element\Template
         parent::__construct($context, $data);
         $this->_customerSession = $customerSession->create();
     }
+
     public function _prepareLayout()
     {
         $this->pageConfig->getTitle()->set(__('Feedback'));
         return parent::_prepareLayout();
         $this->_customerFactory = $customerFactory;
     }
+
     public function getLoggedinCustomerId()
     {
         if ($this->_customerSession->isLoggedIn()) {
