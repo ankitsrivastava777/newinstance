fbce9a45e9457222ac325dc90715d7051e380cde
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-06-01 15:31:43 +0530

    optimized code for autofill email and name.

--- Excellence/Feedback/Block/Index/Index.php
+++ Excellence/Feedback/Block/Index/Index.php
@@ -13,8 +13,6 @@ class Index extends \Magento\Framework\View\Element\Template
      * @param array $data
      */
     protected $_customerSession;
-    protected $_customer;
-
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Customer\Model\SessionFactory $customerSession,
@@ -29,22 +27,10 @@ class Index extends \Magento\Framework\View\Element\Template
     {
         $this->pageConfig->getTitle()->set(__('Feedback'));
         return parent::_prepareLayout();
-        $this->_customerFactory = $customerFactory;
-    }
-
-    public function getLoggedinCustomerId()
-    {
-        if ($this->_customerSession->isLoggedIn()) {
-            return $this->_customerSession->getId();
-        }
-        return false;
     }
 
     public function getCustomerData()
     {
-        if ($this->_customerSession->isLoggedIn()) {
-            return $this->_customerSession->getCustomerData();
-        }
-        return false;
+        return $this->_customerSession;
     }
 }
